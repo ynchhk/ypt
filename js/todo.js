af6a7d766e4785972ef08cf6b37c446b29c663d6
ynchk <ankravcena@gmail.com>
2026-03-22 00:00:26 +0300

    Add localStorage for tasks, custom scrollbar, completed tasks sorting

--- js/todo.js
+++ js/todo.js
@@ -1,4 +1,11 @@
-
+let tasks = []
+const savedTasks = localStorage.getItem('tasks')
+if (savedTasks) {
+    tasks = JSON.parse(savedTasks)
+}
+function saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
 const list = document.querySelector('.todo-list')
 const btnAddTask = document.querySelector('#btn-add')
 const taskNum = document.querySelector('.tasks-num')
@@ -14,12 +21,38 @@ btnAddTask.addEventListener('click', () => {
             <div class="todo-text"></div>
             <span class="todo-time">00:00:00</span>
 `
+
         li.querySelector('.todo-text').textContent = text
         list.appendChild(li)
         taskUpdate()
-
+        tasks.push({text: text, completed: false})
+        saveTasks()
+        li.dataset.index = tasks.length - 1;
     }
 })
+
+function renderTasks() {
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li')
+        li.className = 'todo-item'
+        li.dataset.index = index
+        li.innerHTML = `
+            <input type="checkbox">
+            <div class="todo-text"></div>
+            <span class="todo-time">00:00:00</span>
+        `
+        li.querySelector('.todo-text').textContent = task.text
+        li.querySelector('input').checked = task.completed
+        if (task.completed) {
+            li.querySelector('.todo-text').classList.add('completed')
+        }
+        list.appendChild(li)
+    })
+    countTask = tasks.length
+    taskNum.textContent = `${countTask} tasks for today`
+}
+
+
 function taskUpdate() {
     countTask++
     taskNum.textContent = `${countTask} tasks for today`;
@@ -28,11 +61,24 @@ list.addEventListener('change', (event) => {
     if (event.target.type === 'checkbox'){
         const li = event.target.closest('li')
         const todoText = li.querySelector('.todo-text')
+        const index = li.dataset.index  // добавь
         if (event.target.checked) {
             todoText.classList.add('completed')
-        }
-        else{
+            tasks[index].completed = true
+            list.appendChild(li)// добавь
+        } else {
             todoText.classList.remove('completed')
+            tasks[index].completed = false  // добавь
+            list.prepend(li)
         }
+        saveTasks()
+        // добавь
+
+
     }
 })
+renderTasks()
+
+
+
+
